480ac322e8f9d6b2f534d53ff3b9594b81b01b6f
VVVas <v@vvvas.ru>
2024-03-01 03:37:05 +0300

    готово

--- yacut/forms.py
+++ yacut/forms.py
@@ -2,7 +2,7 @@ from flask_wtf import FlaskForm
 from wtforms import StringField, SubmitField, URLField
 from wtforms.validators import URL, DataRequired, Length, Optional, Regexp
 
-from .constants import SHORT_ID_LENGHT, CUSTOM_ID_PATTERN
+from .constants import CUSTOM_ID_PATTERN
 
 
 class URLMapForm(FlaskForm):
@@ -14,7 +14,7 @@ class URLMapForm(FlaskForm):
     )
     custom_id = StringField(
         'Ваш вариант короткой ссылки',
-        validators=[Length(SHORT_ID_LENGHT, 16, message='Длина короткой ссылки должна быть от %(min)d до %(max)d символов.'),
+        validators=[Length(1, 16, message='Длина короткой ссылки должна быть от %(min)d до %(max)d символов.'),
                     Regexp(regex=CUSTOM_ID_PATTERN, message='Используйте только английские буквы и цифры.'),
                     Optional()]
     )
