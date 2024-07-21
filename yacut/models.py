4642e71cffa7716c23ebe42a706f94b53ef18afe
VVVas <v@vvvas.ru>
2024-07-21 14:53:57 +0300

    Текущее время явно при создании записи

--- yacut/models.py
+++ yacut/models.py
@@ -1,7 +1,13 @@
 """Модели Укоротителя ссылок."""
-from datetime import datetime
+from datetime import datetime, timezone
 
 from . import db
+
+
+def aware_utcnow():
+    """datetime.utcnow deprecated."""
+
+    return datetime.now(timezone.utc)
 
 
 class URLMap(db.Model):
@@ -10,4 +16,5 @@ class URLMap(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     original = db.Column(db.String(256), nullable=False)
     short = db.Column(db.String(16), unique=True, nullable=False)
-    timestamp = db.Column(db.DateTime, default=datetime.utcnow)
+    # timestamp = db.Column(db.DateTime, default=datetime.utcnow)
+    timestamp = db.Column(db.DateTime, default=aware_utcnow)
